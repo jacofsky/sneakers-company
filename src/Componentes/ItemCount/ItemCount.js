a2271252816eb3b1197cff7420a6d3665fb0d3cc
jaco <valentinjacofskyort@gmail.com>
2021-10-03 16:03:29 -0300

    Componentes II - Desafio clase 5

---
+++ src/Componentes/ItemCount/ItemCount.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import './ItemCount.css';
+import { BsPlusCircleFill, BsDashCircleFill } from "react-icons/bs";
+
+
+const ItemCount = ({stock, initial})  => {
+    
+    const [contador, setContador] = useState(initial)
+
+    const addOn = () => {
+        if(contador < stock ) {
+          setContador(contador + 1)
+        }
+    }
+    
+    const decreaseOn = () => {
+        if(contador > 1) {
+            setContador(contador - 1)
+          }
+    }
+
+    return (
+        <div>
+            <div className="contadorBox justify-content-center">
+                <button onClick={decreaseOn} className="dashCircle"><BsDashCircleFill/></button>
+                <p>{contador}</p>
+                <button onClick={addOn} className="plusCircle"><BsPlusCircleFill/></button>
+            </div>
+            <button className="addBolsa">Agregar al la bolsa</button>
+        </div>
+    )
+   
+}
+
+
+
+export default ItemCount
--- src/Componentes/ItemListContainer/ItemListContainer.js
+++ src/Componentes/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import './ItemListContainer.css'
-
+import React from 'react';
+import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount.js';
 
 const ItemListContainer = ({greeting}) => {
+
     return (
         <div>
             <h1 className="greetingStyle">{greeting}</h1>
+            <ItemCount stock={12} initial={1} />
         </div>
     )
 }
